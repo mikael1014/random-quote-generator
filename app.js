34741f9d967010ddeb77c52e28466f720e623686
mikael1014 <mikael1014@hotmail.com>
2023-12-12 12:07:13 +0100

    test 69 quote

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-function quoteGenerator(){
+function quoteGenerator() {
     fetch("https://type.fit/api/quotes")
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
-            //	console.log(data);
+        .then(function (data) {
+            // let randomNumber = Math.floor(Math.random() * 1643);
+            let randomNumber = Math.floor(Math.random() * 69);
 
-            let randomNumber = Math.floor(Math.random()*1643);
+            // Vérifier si data[randomNumber] est défini
+            if (data[randomNumber]) {
+                let quoteText = data[randomNumber].text;
+                let quoteAuthor = data[randomNumber].author || "Unknown Author";
 
-            document.getElementById("quote").innerHTML = data[randomNumber].text;
-            document.getElementById("author").innerHTML = data[randomNumber].author;
+                document.getElementById("quote").innerHTML = quoteText;
+                document.getElementById("author").innerHTML = quoteAuthor;
 
-            //automatically copies quotes to clipboard
-            var text = document.getElementById("quote").innerHTML = data[randomNumber].text;
-            navigator.clipboard.writeText(text).then(function() {
-                alert("Copied to clipboard");
-                //console.log('Async: Copying to clipboard was successful!');
-            }, function(err) {
-                console.error('Async: Could not copy text: ', err);
-            });
-
+                // automatically copies quotes to clipboard
+                var text = quoteText;
+                navigator.clipboard.writeText(text).then(function () {
+                    alert("Copied to clipboard");
+                }, function (err) {
+                    console.error('Async: Could not copy text: ', err);
+                });
+            } else {
+                console.error('Data at index ' + randomNumber + ' is undefined');
+            }
+        })
+        .catch(function (error) {
+            console.error('Error fetching quotes:', error);
         });
 }
 
 quoteGenerator();
 
-function changeQuote(){
+function changeQuote() {
     quoteGenerator();
 }
